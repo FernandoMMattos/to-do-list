935a439bf7340c1e090c389d3f0815b478f244cd
FernandoMMattos <etcloide@gmail.com>
2025-09-08 17:23:16 +0100

    Change api url and fix userService

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import axios, {
 } from "axios";
 import { getToken } from "./userService";
 
-const API_URL = "http://localhost:4000";
+const API_URL = `${window.location.origin}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,7 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:4000";
+import api from "./api";
 
 const loginUser = async (email: string, password: string) => {
-  const res = await axios.post(`${API_URL}/login`, { email, password });
+  const res = await api.post("/login", { email, password });
   const { token, userId } = res.data;
   localStorage.setItem("token", token);
   localStorage.setItem("userId", userId);
@@ -15,24 +13,19 @@ const registerUser = async (
   password: string,
   confirmedPassword: string
 ) => {
-  
   if (!email || !password || !confirmedPassword) {
     throw new Error("All fields are required.");
   }
   if (password !== confirmedPassword) {
     throw new Error("Passwords do not match.");
   }
-  
-  const res = await axios.post(`${API_URL}/register`, { email, password });
+
+  const res = await api.post("/register", { email, password });
   return res.data;
 };
 
-const getUserId = () => {
-  return localStorage.getItem("userId");
-};
+const getUserId = () => localStorage.getItem("userId");
 
-const getToken = () => {
-  return localStorage.getItem("token");
-};
+const getToken = () => localStorage.getItem("token");
 
 export { loginUser, registerUser, getUserId, getToken };
